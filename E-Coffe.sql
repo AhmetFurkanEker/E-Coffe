03f7dc20aad52c0859aeaee395d385aca8a48ced
Ahmet Furkan Eker <56451122+AhmetFurkanEker@users.noreply.github.com>
2023-06-29 20:03:31 +0300

    E-Coffe.sql

--- E-Coffe.sql
+++ E-Coffe.sql
@@ -1,12 +1,12 @@
--- Veritabanýný oluþtur
+-- VeritabanÄ±nÄ± oluÅŸtur
 CREATE DATABASE ECoffe;
 GO
 
--- Ecommerce veritabanýný kullan
+-- Ecommerce veritabanÄ±nÄ± kullan
 USE ECoffe;
 GO
 
--- Users tablosunu oluþtur
+-- Users tablosunu oluÅŸtur
 CREATE TABLE Users (
     UserID INT PRIMARY KEY,
     UserName NVARCHAR(50),
@@ -15,7 +15,7 @@ CREATE TABLE Users (
 );
 GO
 
--- Products tablosunu oluþtur
+-- Products tablosunu oluÅŸtur
 CREATE TABLE Products (
     ProductID INT PRIMARY KEY,
     ProductName NVARCHAR(50),
@@ -24,7 +24,7 @@ CREATE TABLE Products (
 );
 GO
 
--- Orders tablosunu oluþtur
+-- Orders tablosunu oluÅŸtur
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     UserID INT,
@@ -36,7 +36,7 @@ CREATE TABLE Orders (
 );
 GO
 
--- Addresses tablosunu oluþtur
+-- Addresses tablosunu oluÅŸtur
 CREATE TABLE Addresses (
     AddressID INT PRIMARY KEY,
     UserID INT,
@@ -49,7 +49,7 @@ CREATE TABLE Addresses (
 );
 GO
 
--- Payments tablosunu oluþtur
+-- Payments tablosunu oluÅŸtur
 CREATE TABLE Payments (
     PaymentID INT PRIMARY KEY,
     OrderID INT,
@@ -61,7 +61,7 @@ CREATE TABLE Payments (
 );
 GO
 
--- Örnek verileri ekle
+-- Ã–rnek verileri ekle
 INSERT INTO Users (UserID, UserName, Email, RegistrationDate)
 VALUES (1, 'Ahmet Kaya', 'ahmetkaya@gmail.com', '2023-01-01'),
        (2, 'Ahmet Furkan Eker', 'ahmetfurkaneker@gmail.com', '2023-02-01');
@@ -69,7 +69,7 @@ VALUES (1, 'Ahmet Kaya', 'ahmetkaya@gmail.com', '2023-01-01'),
 INSERT INTO Products (ProductID, ProductName, UnitPrice, UnitsInStock)
 VALUES (1, 'Kahve', 10.99, 50),
        (2, 'Latte', 20.99, 100),
-       (3, 'Çay', 5.99, 20),
+       (3, 'Ã‡ay', 5.99, 20),
        (4, 'Americano', 15.99, 75);
 
 INSERT INTO Orders (OrderID, UserID, OrderDate, TotalAmount, PaymentMethod)
@@ -77,22 +77,22 @@ VALUES (1, 1, '2023-03-01', 50.99, 'Credit Card'),
        (2, 2, '2023-03-02', 35.99, 'PayPal');
 
 INSERT INTO Addresses (AddressID, UserID, AddressLine1, AddressLine2, City, PostalCode)
-VALUES (1, 1, 'Osmaniye', 'Merkez', 'Fakýuþaðý', '80000'),
-       (2, 2, 'Adana', 'Merkez', 'Çukurova', '01010');
+VALUES (1, 1, 'Osmaniye', 'Merkez', 'FakÄ±uÅŸaÄŸÄ±', '80000'),
+       (2, 2, 'Adana', 'Merkez', 'Ã‡ukurova', '01010');
 
 INSERT INTO Payments (PaymentID, OrderID, PaymentDate, Amount, PaymentStatus)
 VALUES (1, 1, '2023-03-02', 50.99, 'Completed'),
        (2, 2, '2023-03-03', 35.99, 'Completed');
 GO
 
--- Sipariþ ve kullanýcý bilgilerini birleþtiren karmaþýk bir sorgu
+-- SipariÅŸ ve kullanÄ±cÄ± bilgilerini birleÅŸtiren karmaÅŸÄ±k bir sorgu
 SELECT o.OrderID, o.OrderDate, o.TotalAmount, u.UserName, a.AddressLine1, a.City
 FROM Orders o
 INNER JOIN Users u ON o.UserID = u.UserID
 INNER JOIN Addresses a ON o.UserID = a.UserID;
 GO
 
--- Ürünleri stok durumuna göre sýralayan karmaþýk bir sorgu
+-- ÃœrÃ¼nleri stok durumuna gÃ¶re sÄ±ralayan karmaÅŸÄ±k bir sorgu
 SELECT ProductName, UnitPrice, UnitsInStock,
     CASE
         WHEN UnitsInStock > 50 THEN 'In Stock'
@@ -107,7 +107,7 @@ ORDER BY CASE
 END;
 GO
 
--- Saklý procedure (stored procedure) örneði
+-- SaklÄ± procedure (stored procedure) Ã¶rneÄŸi
 CREATE PROCEDURE GetOrdersByUser
     @UserID INT
 AS
@@ -120,11 +120,11 @@ BEGIN
 END;
 GO
 
--- Saklý procedure'ý çaðýrma
+-- SaklÄ± procedure'Ä± Ã§aÄŸÄ±rma
 EXEC GetOrdersByUser 1;
 GO
 
--- Tablo deðeri iþlevi (table-valued function) örneði
+-- Tablo deÄŸeri iÅŸlevi (table-valued function) Ã¶rneÄŸi
 CREATE FUNCTION GetProductsByPriceRange
     (@MinPrice DECIMAL(10,2),
      @MaxPrice DECIMAL(10,2))
@@ -142,12 +142,12 @@ RETURN (
 );
 GO
 
--- Tablo deðeri iþlevini kullanma
+-- Tablo deÄŸeri iÅŸlevini kullanma
 SELECT *
 FROM dbo.GetProductsByPriceRange(5.00, 15.00);
 GO
 
--- Kýsýtlamalarý ekleme
+-- KÄ±sÄ±tlamalarÄ± ekleme
 ALTER TABLE Users
 ADD CONSTRAINT CK_Users_Email CHECK (Email LIKE '%@%');
 
